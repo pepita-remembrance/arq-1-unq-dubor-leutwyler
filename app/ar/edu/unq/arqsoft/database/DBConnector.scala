4dc78c61471c699be5c29466e21fb2b7102d08eb
emanueldubor <emanueldubor@gmail.com>
2018-04-24 18:08:47 -0300

    Added docker-compose file.

--- app/ar/edu/unq/arqsoft/database/DBConnector.scala
+++ app/ar/edu/unq/arqsoft/database/DBConnector.scala
@@ -14,9 +14,14 @@ trait DBConnector {
 @Singleton
 class PostgresConnector @Inject()(configuration: Configuration) extends DBConnector {
   def sessionCreator: Option[() => AbstractSession] = {
-    val dbConfig = configuration.get[Configuration]("app.database")
+    val databaseUrl = System.getenv("JDBC_DATABASE_URL") match {
+      case null =>
+        val dbConfig = configuration.get[Configuration]("app.database")
+        s"jdbc:postgresql://${dbConfig.get[String]("host")}/${dbConfig.get[String]("db")}?user=${dbConfig.get[String]("user")}&password=${dbConfig.get[String]("password")}"
+      case value => value
+    }
     Some(() => Session.create(
-      DriverManager.getConnection(s"jdbc:postgresql://${dbConfig.get[String]("host")}/${dbConfig.get[String]("db")}?user=${dbConfig.get[String]("user")}&password=${dbConfig.get[String]("password")}"),
+      DriverManager.getConnection(databaseUrl),
       new PostgreSqlAdapter
     ))
   }
