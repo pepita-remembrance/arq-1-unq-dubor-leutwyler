6f2266a3e9d82681aa458f813b0ae7e927942c78
emanueldubor <emanueldubor@gmail.com>
2018-04-26 11:13:07 -0300

    Simplified app db connection configuration.

--- app/ar/edu/unq/arqsoft/database/DBConnector.scala
+++ app/ar/edu/unq/arqsoft/database/DBConnector.scala
@@ -14,14 +14,8 @@ trait DBConnector {
 @Singleton
 class PostgresConnector @Inject()(configuration: Configuration) extends DBConnector {
   def sessionCreator: Option[() => AbstractSession] = {
-    val databaseUrl = System.getenv("JDBC_DATABASE_URL") match {
-      case null =>
-        val dbConfig = configuration.get[Configuration]("app.database")
-        s"jdbc:postgresql://${dbConfig.get[String]("host")}/${dbConfig.get[String]("db")}?user=${dbConfig.get[String]("user")}&password=${dbConfig.get[String]("password")}"
-      case value => value
-    }
     Some(() => Session.create(
-      DriverManager.getConnection(databaseUrl),
+      DriverManager.getConnection(configuration.get[String]("app.database.JDBCurl")),
       new PostgreSqlAdapter
     ))
   }
