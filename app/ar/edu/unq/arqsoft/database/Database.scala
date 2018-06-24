c956f7927b136080d45b1adf70795d0c7ce42550
emanueldubor <emanueldubor@gmail.com>
2018-06-24 19:03:30 -0300

    Akka thread poll config and seed for stress improvements.

--- app/ar/edu/unq/arqsoft/database/Database.scala
+++ app/ar/edu/unq/arqsoft/database/Database.scala
@@ -1,16 +1,27 @@
 package ar.edu.unq.arqsoft.database
 
+import akka.actor.ActorSystem
 import com.google.inject.{Inject, Singleton}
 import org.joda.time.DateTimeZone
 import org.squeryl.SessionFactory
+import play.api.Configuration
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 @Singleton
-class Database @Inject()(connector: DBConnector)
+class Database @Inject()(connector: DBConnector, configuration: Configuration, actorSystem: ActorSystem)
   extends DemoDatabase
     with SeedData {
 
   DateTimeZone.setDefault(DateTimeZone.forOffsetHours(-3)) // Buenos Aires
   SessionFactory.concreteFactory = connector.sessionCreator
+
+  actorSystem.scheduler.scheduleOnce(10 seconds) {
+    if (configuration.get[Boolean]("db.default.stress.active")) {
+      seedForStress(configuration.get[Option[Int]]("db.default.stress.amount"))
+    }
+  }
 
   override def seed(): Unit = {
     init()
